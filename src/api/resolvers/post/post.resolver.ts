e27f951763adb59a3d02f310b6422c4cc921f2da
raman32 <rg.raman.ghimire@gmail.com>
2021-05-31 20:55:11 +0545

    added prisma relay cursor connection

--- src/api/resolvers/post/post.resolver.ts
+++ src/api/resolvers/post/post.resolver.ts
@@ -1,9 +1,12 @@
 import { Post as Post_ } from '.prisma/client';
+import { User as User_ } from '.prisma/client';
 import { UnauthorizedException, UseGuards } from '@nestjs/common';
-import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
+import { Args, Mutation, Parent, Query, ResolveField, Resolver } from '@nestjs/graphql';
+import { PaginationArgs } from '../../../common/pagination/pagination.args';
 import { CreatePostInput } from '../../../models/inputs/createPost.input';
+import { PostConnection } from '../../../models/pagination/post-connection';
 import { Post } from '../../../models/post.model';
-import { UserRole } from '../../../models/user.model';
+import { User, UserRole } from '../../../models/user.model';
 import { PostService } from '../../../services/post.service';
 import { PrismaService } from '../../../services/prisma.service';
 import { RequestContext } from '../../common/requestContext';
@@ -12,16 +15,31 @@ import { Roles } from '../../decorators/role.decorator';
 import { AuthenticatedSessionGuard } from '../auth/guards/auth.guard';
 import { GQLGuard } from '../auth/guards/gql.guard';
 import { RolesGuard } from '../auth/guards/role.guard';
+import { Connection, Edge, findManyCursorConnection } from '@devoxa/prisma-relay-cursor-connection';
 
 @Resolver(of => Post)
 @UseGuards(GQLGuard)
 export class PostResolver {
     constructor(
-        private readonly postService: PostService) { }
+        private readonly postService: PostService,
+        private readonly prisma: PrismaService) { }
 
     @Query(returns => Post)
     async getPost(@Args('id', { type: () => String }) id: string): Promise<Post_> {
         return this.postService.getSinglePost(id)
+    }
+
+    @Query(returns => PostConnection)
+    async getPosts(@Args() { after, before, first, last }: PaginationArgs): Promise<Connection<Post_, Edge<Post_>>> {
+        const postCursors = findManyCursorConnection(
+            (args) =>
+                this.prisma.post.findMany({
+                    ...args,
+                }),
+            () => this.prisma.post.count(),
+            { first, last, before, after },
+        );
+        return postCursors;
     }
 
     @Mutation(returns => Post)
@@ -38,6 +56,12 @@ export class PostResolver {
             throw new UnauthorizedException();
         }
         return this.postService.createPost({ type: input.type, body: input.body, slug: input.slug, title: input.title, url: input.url, userId: context.user.id })
+    }
+
+    @ResolveField('user', returns => User)
+    async user(@Parent() post: Post): Promise<User_> {
+        const { userId } = post;
+        return this.prisma.user.findUnique({ where: { id: userId } })
     }
 
 }
--- src/api/resolvers/user/user.resolver.ts
+++ src/api/resolvers/user/user.resolver.ts
@@ -1,15 +1,20 @@
-import { Query, ResolveField, Resolver } from '@nestjs/graphql';
+import { Args, Parent, Query, ResolveField, Resolver } from '@nestjs/graphql';
 import { User, UserRole } from '../../../models/user.model';
 import { UseGuards } from '@nestjs/common';
 import { GQLGuard } from '../auth/guards/gql.guard';
 import { PrismaService } from '../../../services/prisma.service';
-import { User as UserType } from 'prisma/prisma-client'
+import { User as User_ } from 'prisma/prisma-client'
+import { Post as Post_ } from 'prisma/prisma-client'
 import { UserService } from '../../../services/user.service';
 import { RequestContext } from '../../common/requestContext';
 import { Ctx } from '../../decorators/requestContext.decorator';
 import { Roles } from '../../decorators/role.decorator';
 import { AuthenticatedSessionGuard } from '../auth/guards/auth.guard';
 import { RolesGuard } from '../auth/guards/role.guard';
+import { Post } from '../../../models/post.model';
+import { UserConnection } from '../../../models/pagination/user-connection';
+import { PaginationArgs } from '../../../common/pagination/pagination.args';
+import { Connection, Edge, findManyCursorConnection } from '@devoxa/prisma-relay-cursor-connection';
 
 @Resolver((of) => User)
 @UseGuards(GQLGuard)
@@ -22,17 +27,29 @@ export class UserResolver {
 
     @Query((returns) => User, { nullable: true })
     @UseGuards(AuthenticatedSessionGuard)
-    async me(@Ctx() context: RequestContext,): Promise<UserType> {
+    async me(@Ctx() context: RequestContext,): Promise<User_> {
         return context.user;
     }
 
-    @Query((returns) => [User], { nullable: true })
+    @Query((returns) => UserConnection)
     @Roles(UserRole.admin, UserRole.moderator)
     @UseGuards(RolesGuard)
-    async getUsers(): Promise<UserType[]> {
-        return this.prisma.user.findMany();
+    async getUsers(@Args() { after, before, first, last }: PaginationArgs): Promise<Connection<User_, Edge<User_>>> {
+        const userCursors = findManyCursorConnection(
+            (args) =>
+                this.prisma.user.findMany({
+                    ...args,
+                }),
+            () => this.prisma.post.count(),
+            { first, last, before, after },
+        );
+        return userCursors;
     }
 
-
+    @ResolveField('posts', (returns) => [Post], { nullable: true })
+    async posts(@Parent() user: User): Promise<Post_[]> {
+        const { id } = user;
+        return this.prisma.post.findMany({ where: { userId: id } })
+    }
 
 }
