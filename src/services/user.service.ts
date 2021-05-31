f8a46be399a7e973daa6a287dfc8b250ffd664f9
raman32 <rg.raman.ghimire@gmail.com>
2021-05-31 19:30:57 +0545

    fixed authorization

--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -1,12 +1,19 @@
 import { User } from '.prisma/client';
 import { Injectable } from '@nestjs/common';
+import { CreateUserInput } from '../models/inputs/createUser.input';
 import { UpdateUserInput } from '../models/inputs/updateUser.input';
 import { PrismaService } from './prisma.service';
 
 @Injectable()
 export class UserService {
     constructor(private readonly prisma: PrismaService) { }
-
+    async createUser(data: CreateUserInput): Promise<User> {
+        return this.prisma.user.create({
+            data: {
+                ...data,
+            },
+        });
+    }
     async updateUser(data: UpdateUserInput, userId: string): Promise<User> {
         return this.prisma.user.update({
             where: {
